95c37014640fd8351a8ca7698b2e41051b7187c0
Andy Roulin <andy.roulin@epfl.ch>
2013-09-24 11:33:22 +0200

    Completes 3. Entry Point but Checkstyle fails

---
+++ SwEng2013QuizApp/src/epfl/sweng/entry/MainActivity.java
@@ -0,0 +1,30 @@
+package epfl.sweng.entry;
+
+import epfl.sweng.R;
+import epfl.sweng.testing.TestingTransactions;
+import epfl.sweng.testing.TestingTransactions.TTChecks;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+/**
+ * TODO Commentaire Javadoc
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		TestingTransactions.check(TTChecks.MAIN_ACTIVITY_SHOWN);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
