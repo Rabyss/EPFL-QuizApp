f76202d16d6a0488f853005ac2897e864ddd2958
Philémon Favrod <philemon.favrod@epfl.ch>
2013-09-26 15:06:29 +0200

    Add factory for quiz question and server request.

--- SwEng2013QuizApp/src/epfl/sweng/QuizQuestionModel.java
+++ SwEng2013QuizApp/src/epfl/sweng/QuizQuestionModel.java
@@ -1,7 +1,11 @@
 package epfl.sweng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /** Represents a question of the quiz.
- *  @author Philémon Favrod (philemon.favrod@epfl.ch)
+ *  @author Philemon Favrod (philemon.favrod@epfl.ch)
  */
 public class QuizQuestionModel {
     private final int mId;
@@ -12,35 +16,55 @@ public class QuizQuestionModel {
     private final String mOwner;
     
     public QuizQuestionModel(int id, String question, String[] answers, int solutionIndex, String[] tags, String owner) {
-	mId = id;
-	mQuestion = question;
-	mAnswers = answers;
-	mSolutionIndex = solutionIndex;
-	mTags = tags;
-	mOwner = owner;
+        mId = id;
+        mQuestion = question;
+        mAnswers = answers;
+        mSolutionIndex = solutionIndex;
+        mTags = tags;
+	    mOwner = owner;
     }
 
+    public QuizQuestionModel(JSONObject jsonModel) throws JSONException {
+        mId = jsonModel.getInt("id");
+        mQuestion = jsonModel.getString("question");
+        mAnswers = extractArrayFromJson(jsonModel.getJSONArray("answers"));
+        mSolutionIndex = jsonModel.getInt("solutionIndex");
+        mTags = extractArrayFromJson(jsonModel.getJSONArray("tags"));
+        mOwner = jsonModel.getString("owner");
+    }
+    
+    private static String[] extractArrayFromJson(JSONArray jsonArray) throws JSONException {
+        String[] stringArray = new String[jsonArray.length()];
+        for (int i = 0; i < stringArray.length; i++) {
+            stringArray[i] = jsonArray.getString(i);
+        }
+        return stringArray;
+    }
+    
     public int getId() {
-	return mId;
+        return mId;
     }
 
     public String getQuestion() {
-	return mQuestion;
+        return mQuestion;
     }
 
     public String[] getAnsers() {
-	return mAnswers;
+        return mAnswers;
     }
 
     /** Checks whether the given index is the index of the solution.
     */
     public boolean isSolution(int index) {
-	return (mSolutionIndex == index);
+        return (mSolutionIndex == index);
     }
 
     public String[] getTags() {
-	return mTags;
+        return mTags;
     }
 
+    public String getOwner() {
+        return mOwner;
+    }
     
 }
