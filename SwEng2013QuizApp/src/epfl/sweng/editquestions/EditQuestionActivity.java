e2131b5d15ff060e0e49e7b642a31b5d36d4323e
Andy Roulin <andy.roulin@epfl.ch>
2013-09-27 15:09:35 +0200

    Advances in construction of GUI

--- SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 /**
  * 
@@ -23,7 +25,7 @@ public class EditQuestionActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_question);
 		
-		
+		createNewAnswer((ViewGroup) findViewById(R.id.linearLayoutAnswers), true);
 		
 		// let the testing infrastructure know that edit question has been initialized
 		TestingTransactions.check(TTChecks.EDIT_QUESTIONS_SHOWN);
@@ -38,15 +40,36 @@ public class EditQuestionActivity extends Activity {
 	
 	public void addAnswer(View view) {
 		ViewGroup linearLayout = (ViewGroup) findViewById(R.id.linearLayoutAnswers);
-		createNewAnswer(linearLayout);
+		createNewAnswer(linearLayout, false);
 	}
 	
-	public void createNewAnswer(ViewGroup view) {
-		Button btn = new Button(this);
-		btn.setText("test");
+	public void createNewAnswer(ViewGroup view, boolean correct) {
+		// Create Layout
+		LinearLayout linearLayout = new LinearLayout(this);
+		linearLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+		view.addView(linearLayout);
 		
-		LayoutParams btnLayout = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		view.addView(btn, btnLayout);
+		// Create EditText
+		EditText editText = new EditText(this);
+		editText.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		editText.setHint(R.string.edit_answer_text);
+		linearLayout.addView(editText);
+		
+		// Create Button Correct
+		Button btn = new Button(this);
+		if (correct) {
+			btn.setText(R.string.button_check);
+			btn.setEnabled(false);
+		} else {
+			btn.setText(R.string.button_cross);
+		}
+		btn.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		linearLayout.addView(btn);
+		
+		// Create Button delete
+		Button btnDelete = new Button(this);
+		btnDelete.setText(R.string.button_delete);
+		linearLayout.addView(btnDelete);
 	}
 
 }
