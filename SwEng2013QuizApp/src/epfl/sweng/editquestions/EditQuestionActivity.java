e2b83adede71a2889783a16aec2dd8d288495c15
Andy Roulin <andy.roulin@epfl.ch>
2013-09-27 14:08:07 +0200

    Ignore log files

--- SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -1,9 +1,15 @@
 package epfl.sweng.editquestions;
 
 import epfl.sweng.R;
+import epfl.sweng.testing.TestingTransactions;
+import epfl.sweng.testing.TestingTransactions.TTChecks;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 
 /**
  * 
@@ -16,6 +22,11 @@ public class EditQuestionActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_question);
+		
+		
+		
+		// let the testing infrastructure know that edit question has been initialized
+		TestingTransactions.check(TTChecks.EDIT_QUESTIONS_SHOWN);
 	}
 
 	@Override
@@ -23,6 +34,19 @@ public class EditQuestionActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.edit_question, menu);
 		return true;
+	}
+	
+	public void addAnswer(View view) {
+		ViewGroup linearLayout = (ViewGroup) findViewById(R.id.linearLayoutAnswers);
+		createNewAnswer(linearLayout);
+	}
+	
+	public void createNewAnswer(ViewGroup view) {
+		Button btn = new Button(this);
+		btn.setText("test");
+		
+		LayoutParams btnLayout = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		view.addView(btn, btnLayout);
 	}
 
 }
