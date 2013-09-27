40871b969be3815ecd2e5120cb6b664f7b0e26f7
Andy Roulin <andy.roulin@epfl.ch>
2013-09-27 17:23:02 +0200

    Enhances EditQuestion Part

--- SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -1,5 +1,7 @@
 package epfl.sweng.editquestions;
 
+import java.util.ArrayList;
+
 import epfl.sweng.R;
 import epfl.sweng.testing.TestingTransactions;
 import epfl.sweng.testing.TestingTransactions.TTChecks;
@@ -8,10 +10,6 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 
 /**
  * 
@@ -19,13 +17,16 @@ import android.widget.LinearLayout;
  *
  */
 public class EditQuestionActivity extends Activity {
+	
+	private ArrayList<Answer> answers;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_question);
 		
-		createNewAnswer((ViewGroup) findViewById(R.id.linearLayoutAnswers), true);
+		answers = new ArrayList<Answer>();
+		answers.add(new Answer(this, (ViewGroup) findViewById(R.id.linearLayoutAnswers), true));
 		
 		// let the testing infrastructure know that edit question has been initialized
 		TestingTransactions.check(TTChecks.EDIT_QUESTIONS_SHOWN);
@@ -40,36 +41,11 @@ public class EditQuestionActivity extends Activity {
 	
 	public void addAnswer(View view) {
 		ViewGroup linearLayout = (ViewGroup) findViewById(R.id.linearLayoutAnswers);
-		createNewAnswer(linearLayout, false);
+		answers.add(new Answer(this, linearLayout, false));
 	}
 	
-	public void createNewAnswer(ViewGroup view, boolean correct) {
-		// Create Layout
-		LinearLayout linearLayout = new LinearLayout(this);
-		linearLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-		view.addView(linearLayout);
-		
-		// Create EditText
-		EditText editText = new EditText(this);
-		editText.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		editText.setHint(R.string.edit_answer_text);
-		linearLayout.addView(editText);
-		
-		// Create Button Correct
-		Button btn = new Button(this);
-		if (correct) {
-			btn.setText(R.string.button_check);
-			btn.setEnabled(false);
-		} else {
-			btn.setText(R.string.button_cross);
-		}
-		btn.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		linearLayout.addView(btn);
-		
-		// Create Button delete
-		Button btnDelete = new Button(this);
-		btnDelete.setText(R.string.button_delete);
-		linearLayout.addView(btnDelete);
+	public ArrayList<Answer> getAnswers() {
+		return answers;
 	}
 
 }
