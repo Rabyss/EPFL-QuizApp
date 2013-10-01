4dad2195318b3633f45fe8de9643605f15e06f96
Andy Roulin <andy.roulin@epfl.ch>
2013-10-01 11:50:51 +0200

    Close keyBoard when clicking submit

--- SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -11,33 +11,37 @@ import epfl.sweng.testing.TestingTransactions;
 import epfl.sweng.testing.TestingTransactions.TTChecks;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
 /**
  * 
  * Activity to edit questions
- *
+ * 
  */
 public class EditQuestionActivity extends Activity {
-	
+
 	private ArrayList<Answer> answers;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_question);
-		
+
 		answers = new ArrayList<Answer>();
-		answers.add(new Answer(this, (ViewGroup) findViewById(R.id.linearLayoutAnswers), true));
-		
+		answers.add(new Answer(this,
+				(ViewGroup) findViewById(R.id.linearLayoutAnswers), true));
+
 		findViewById(R.id.editQuestionText).requestFocus();
-		
-		// let the testing infrastructure know that edit question has been initialized
+
+		// let the testing infrastructure know that edit question has been
+		// initialized
 		TestingTransactions.check(TTChecks.EDIT_QUESTIONS_SHOWN);
 	}
 
@@ -47,39 +51,47 @@ public class EditQuestionActivity extends Activity {
 		getMenuInflater().inflate(R.menu.edit_question, menu);
 		return true;
 	}
-	
+
 	public void addAnswer(View view) {
 		ViewGroup linearLayout = (ViewGroup) findViewById(R.id.linearLayoutAnswers);
 		answers.add(new Answer(this, linearLayout, false));
 	}
-	
+
 	public ArrayList<Answer> getAnswers() {
 		return answers;
 	}
-	
+
 	public void submitQuestion(View view) {
-		Toast.makeText(this, "Submitting", 1000).show();
-		
-		String question = ((EditText) findViewById(R.id.editQuestionText)).getText().toString();
-		
+		InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+		inputManager.hideSoftInputFromWindow(
+				getCurrentFocus().getWindowToken(),
+				InputMethodManager.HIDE_NOT_ALWAYS);
+
+		String question = ((EditText) findViewById(R.id.editQuestionText))
+				.getText().toString();
+
 		ArrayList<String> answersText = new ArrayList<String>();
 		int solutionIndex = 0;
-		
+
 		for (Answer answer : answers) {
 			answersText.add(answer.getContent());
 			if (answer.isCorrect()) {
 				solutionIndex = answers.indexOf(answer);
 			}
 		}
-		
-		String tagsText = ((EditText) findViewById(R.id.editTags)).getText().toString();
+
+		String tagsText = ((EditText) findViewById(R.id.editTags)).getText()
+				.toString();
 		String[] tags = tagsText.split("\\W+");
-		
-		QuizQuestion quizQuestion = new QuizQuestion(null, question, answersText.toArray(new String[answers.size()]),
-				solutionIndex, tags, null);
-		
+
+		QuizQuestion quizQuestion = new QuizQuestion(null, question,
+				answersText.toArray(new String[answers.size()]), solutionIndex,
+				tags, null);
+
 		try {
-			System.out.println(ServerCommunicator.getInstance().submitQuizQuestion(quizQuestion));
+			System.out.println(ServerCommunicator.getInstance()
+					.submitQuizQuestion(quizQuestion));
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -90,10 +102,11 @@ public class EditQuestionActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		Toast.makeText(this, "Successful Submit", 1000).show();
-		Intent displayActivityIntent = new Intent(this, ShowQuestionsActivity.class);
-	    startActivity(displayActivityIntent);
+		Intent displayActivityIntent = new Intent(this,
+				ShowQuestionsActivity.class);
+		startActivity(displayActivityIntent);
 	}
 
 }
