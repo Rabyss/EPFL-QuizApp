7c8b6ed0096dbf382b111cf7422155bdace2288a
Elieva <elieva.pignat@epfl.ch>
2013-10-01 11:24:50 +0200

    Refactoring ShowQuestionsActivity

--- SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
@@ -1,10 +1,9 @@
 package epfl.sweng.showquestions;
 
-import java.io.IOException;
+
 import java.util.concurrent.ExecutionException;
 
-import org.apache.http.client.ClientProtocolException;
-import org.json.JSONException;
+
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -28,11 +27,11 @@ import epfl.sweng.testing.TestingTransactions.TTChecks;
 public class ShowQuestionsActivity extends Activity {
 
     private QuizQuestion mRandomQuestion = null;
-    private Button[] answer;
-    private Button nextQuestion;
-    private TextView[] correctness;
-    private int indexButton;
-    private LinearLayout linearLayout;
+    private Button[] mAnswer;
+    private Button mNextQuestion;
+    private TextView[] mCorrectness;
+    private int mIndexButton;
+    private LinearLayout mLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +51,9 @@ public class ShowQuestionsActivity extends Activity {
      */
     public void updateQuestion() {
         // creates the main layout
-        linearLayout = new LinearLayout(this);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        linearLayout.setLayoutParams(new LayoutParams(
+        mLinearLayout = new LinearLayout(this);
+        mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        mLinearLayout.setLayoutParams(new LayoutParams(
                 LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         // uploads a random question from the server
         try {
@@ -66,19 +65,16 @@ public class ShowQuestionsActivity extends Activity {
         } catch (ExecutionException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        } catch (AssertionError e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        } 
         // initializes the title of the question
         TextView question = new TextView(this);
         question.setText(mRandomQuestion.getQuestion());
-        linearLayout.addView(question);
+        mLinearLayout.addView(question);
         // initializes the button nextQuestion
-        nextQuestion = new Button(this);
-        nextQuestion.setText("Next Question");
-        nextQuestion.setEnabled(false);
-        nextQuestion.setOnClickListener(new OnClickListener() {
+        mNextQuestion = new Button(this);
+        mNextQuestion.setText("Next Question");
+        mNextQuestion.setEnabled(false);
+        mNextQuestion.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -88,62 +84,62 @@ public class ShowQuestionsActivity extends Activity {
 
         displayAnswer();
 
-        linearLayout.addView(nextQuestion);
+        mLinearLayout.addView(mNextQuestion);
         displayTags();
         displaySolutionIndex();
-        setContentView(linearLayout);
+        setContentView(mLinearLayout);
         TestingTransactions.check(TTChecks.QUESTION_SHOWN);
     }
 
     public void displayAnswer() {
         int totalAnswer = mRandomQuestion.getAnswers().length;
-        answer = new Button[totalAnswer];
-        correctness = new TextView[totalAnswer];
+        mAnswer = new Button[totalAnswer];
+        mCorrectness = new TextView[totalAnswer];
         // initializes all the buttons of answer
-        for (indexButton = 0; indexButton < totalAnswer; indexButton++) {
+        for (mIndexButton = 0; mIndexButton < totalAnswer; mIndexButton++) {
             LinearLayout answerLayout = new LinearLayout(this);
             answerLayout.setOrientation(LinearLayout.HORIZONTAL);
             answerLayout.setLayoutParams(new LayoutParams(
                     LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
-            correctness[indexButton] = new TextView(this);
-            if (mRandomQuestion.isSolution(indexButton)) {
-                correctness[indexButton].setText("\u2714");
+            mCorrectness[mIndexButton] = new TextView(this);
+            if (mRandomQuestion.isSolution(mIndexButton)) {
+                mCorrectness[mIndexButton].setText("\u2714");
             } else {
-                correctness[indexButton].setText("\u2718");
+                mCorrectness[mIndexButton].setText("\u2718");
             }
-            correctness[indexButton].setVisibility(View.INVISIBLE);
-            answer[indexButton] = new Button(this);
-            answer[indexButton].setClickable(true);
-            answer[indexButton]
-                    .setText(mRandomQuestion.getAnswers()[indexButton]);
-            answer[indexButton].setOnClickListener(new View.OnClickListener() {
+            mCorrectness[mIndexButton].setVisibility(View.INVISIBLE);
+            mAnswer[mIndexButton] = new Button(this);
+            mAnswer[mIndexButton].setClickable(true);
+            mAnswer[mIndexButton]
+                    .setText(mRandomQuestion.getAnswers()[mIndexButton]);
+            mAnswer[mIndexButton].setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View v) {
 
                     int actualAnswer = 0;
                     for (int i = 0; i < mRandomQuestion.getAnswers().length; i++) {
-                        if (v == answer[i]) {
+                        if (v == mAnswer[i]) {
                             actualAnswer = i;
                         }
                     }
-                    correctness[actualAnswer].setVisibility(View.VISIBLE);
+                    mCorrectness[actualAnswer].setVisibility(View.VISIBLE);
                     if (mRandomQuestion.isSolution(actualAnswer)) {
-                        nextQuestion.setEnabled(true);
+                        mNextQuestion.setEnabled(true);
                         for (int i = 0; i < mRandomQuestion.getAnswers().length; i++) {
-                            answer[i].setEnabled(false);
+                            mAnswer[i].setEnabled(false);
                             if (i != actualAnswer) {
-                                correctness[i].setVisibility(View.INVISIBLE);
+                                mCorrectness[i].setVisibility(View.INVISIBLE);
                             }
                         }
                     }
                     TestingTransactions.check(TTChecks.ANSWER_SELECTED);
                 }
             });
-            answerLayout.addView(answer[indexButton]);
-            answerLayout.addView(correctness[indexButton]);
-            linearLayout.addView(answerLayout);
+            answerLayout.addView(mAnswer[mIndexButton]);
+            answerLayout.addView(mCorrectness[mIndexButton]);
+            mLinearLayout.addView(answerLayout);
         }
     }
 
@@ -152,7 +148,7 @@ public class ShowQuestionsActivity extends Activity {
         for (int i = 0; i < totalTags; i++) {
             TextView tagText = new TextView(this);
             tagText.setText(mRandomQuestion.getTags()[i]);
-            linearLayout.addView(tagText);
+            mLinearLayout.addView(tagText);
         }
 
     }
@@ -162,6 +158,6 @@ public class ShowQuestionsActivity extends Activity {
         int index = mRandomQuestion.getSolutionIndex();
         String solutionText = String.valueOf(index);
         solutionIndex.setText(solutionText);
-        linearLayout.addView(solutionIndex);
+        mLinearLayout.addView(solutionIndex);
     }
 }
