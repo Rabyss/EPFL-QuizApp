982dedef0d9409f3047a7524c003e4b637743cd7
Jérémy Rabasco <jeremy.rabasco@epfl.ch>
2013-10-11 18:15:57 +0200

    Added a new model that represents the parameters of an HTTP request.

---
+++ SwEng2013QuizApp/src/epfl/sweng/servercomm/RequestContext.java
@@ -0,0 +1,47 @@
+package epfl.sweng.servercomm;
+
+import java.util.HashMap;
+
+import org.apache.http.HttpEntity;
+
+/**
+ * 
+ * Class that contains all the parameters
+ * 
+ */
+public class RequestContext {
+
+    private String mServerURL;
+    private HashMap<String, String> mHeaders;
+    private HttpEntity mEntity;
+
+    public RequestContext(String serverURL, HttpEntity entity) {
+        mServerURL = serverURL;
+        mEntity = entity;
+        mHeaders = new HashMap<String, String>();
+    }
+
+    public RequestContext(String serverURL) {
+        mServerURL = serverURL;
+        mEntity = null;
+        mHeaders = new HashMap<String, String>();
+    }
+
+    public void addHeader(String name, String value) {
+        mHeaders.put(name, value);
+    }
+
+    public String getServerURL() {
+        return mServerURL;
+    }
+
+    public HttpEntity getEntity() {
+        return mEntity;
+    }
+
+    @SuppressWarnings("unchecked")
+    public HashMap<String, String> getHeaders() {
+        return (HashMap<String, String>) mHeaders.clone();
+    }
+
+}
