5e636426700550376278e0566d9444ca0ed60043
Sacha Bron <sacha.bron@copperflake.com>
2013-10-15 17:30:59 +0200

    Removing old boolean.

--- SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -101,8 +101,6 @@ public class EditQuestionActivity extends QuestionActivity {
         }
 
         showProgressDialog();
-        setWaiting(true);
-
     }
     
     public void on(PostedQuestionEvent event) {
