438a6e6abc2a153ff862701d4f5a67e89d99aa6e
Elieva <elieva.pignat@epfl.ch>
2013-10-16 11:57:34 +0200

    Creation of the class for testing the main activity and the authentication activity

---
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/AuthenticationActivityTest.java
@@ -0,0 +1,14 @@
+package epfl.sweng.test;
+
+import epfl.sweng.authentication.AuthenticationActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class AuthenticationActivityTest extends ActivityInstrumentationTestCase2<AuthenticationActivity> {
+
+	public AuthenticationActivityTest(
+			Class<AuthenticationActivity> activityClass) {
+		super(activityClass);
+	}
+
+}
---
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/MainActivityTest.java
@@ -0,0 +1,33 @@
+package epfl.sweng.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import epfl.sweng.entry.MainActivity;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity>{
+
+	public MainActivityTest(Class<MainActivity> activityClass) {
+		super(activityClass);
+	}
+	/**
+	 * Test the main activity if an user is not logged
+	 * The submit button and the show question button must be disabled
+	 */
+	public void unLoggedUserTest(){
+		
+	}
+	/**
+	 * Test the main activity if an user is logged
+	 */
+	public void loggedUserTest(){
+		
+	}
+	/**
+	 * Test the main activity if an user push the log out button
+	 */
+	public void logOutTest(){
+		
+	}
+	
+
+
+}
