a62433483957d412037956dc44d2d73596c1ddea
Elieva <elieva.pignat@epfl.ch>
2013-10-18 16:31:10 +0200

    Joy: the authentication test make an correct authentication

--- SwEng2013QuizAppTest/src/epfl/sweng/test/AuthenticationActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/AuthenticationActivityTest.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpStatus;
 import com.jayway.android.robotium.solo.Solo;
 
 import epfl.sweng.authentication.AuthenticationActivity;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.test.minimalmock.MockHttpClient;
 import epfl.sweng.testing.TestCoordinator;
 import epfl.sweng.testing.TestCoordinator.TTChecks;
@@ -25,6 +26,7 @@ public class AuthenticationActivityTest extends ActivityInstrumentationTestCase2
 	protected void setUp() {
 		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
 		solo = new Solo(getInstrumentation(), getActivity());
+		
 	}
 	/**
 	 * Test if the AuthenticationActivity is correctly initialized
@@ -39,26 +41,51 @@ public class AuthenticationActivityTest extends ActivityInstrumentationTestCase2
 	 * Test if the fields are cleared after a bad authentication
 	 */
 	public void testBadAuthenticationDisplaying(){
+		httpClient= new MockHttpClient();
+		SwengHttpClientFactory.setInstance(httpClient);
+		httpClient.pushCannedResponse("/*/", 400, 
+				null, "application/json");
 		EditText username= solo.getEditText("GASPAR Username");
 		EditText password = solo.getEditText("GASPAR Password");
 		solo.typeText(username, "WhiteSnow");
 		solo.typeText(password, "SevenDwarfs");
 		solo.clickOnButton("Log in using Tequila");
 		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
-		assertFalse("GASPAR Username must be empty", solo.searchEditText("WhiteSnow"));
+		assertFalse("GASPAR Username must be empty", solo.searchEditText("SnowWhite"));
 		assertFalse("GASPAR Password must be empty", solo.searchEditText("SevenDwarfs"));
 		assertTrue("Button log in must be displayed", solo.searchButton("Log in using Tequila"));
 		assertTrue("Button log in must be enabled", solo.getButton("Log in using Tequila").isEnabled());
 	}
 	
 	public void testCorrectAuthenticationDisplaying(){
+		httpClient= new MockHttpClient();
+		SwengHttpClientFactory.setInstance(httpClient);
+		
+		httpClient.pushCannedResponse("POST https://sweng-quiz.appspot.com/login", 200, 
+				"{\"session\": \"<random_string>\","
+			 +" \"message\": \"Here's your session id. Please include the following HTTP"
+			 +"             header in your subsequent requests:\n"
+			 +"            Authorization: Tequila <random_string>\"}", "application/json");
+		
+		httpClient.pushCannedResponse("POST https://tequila.epfl.ch/cgi-bin/tequila/login", 302, "{"
+				+"  \"token\": \"rqtvk5d3za2x6ocak1a41dsmywogrdlv5\","
+				+"  \"message\": \"Here's your authentication token. Please validate it "
+				+"              with Tequila at https://tequila.epfl.ch/cgi-bin/tequila/login\" }",
+				"application/json");	
+		
+		
+		httpClient.pushCannedResponse("GET.*https://sweng-quiz.appspot.com/login", 
+				200, "{"
+				+"  \"token\": \"rqtvk5d3za2x6ocak1a41dsmywogrdlv5\","
+				+"  \"message\": \"Here's your authentication token. Please validate it "
+				+"              with Tequila at https://tequila.epfl.ch/cgi-bin/tequila/login\" }",
+				"application/json");
 		EditText username= solo.getEditText("GASPAR Username");
 		EditText password = solo.getEditText("GASPAR Password");
-		solo.typeText(username, "WhiteSnow");
+		solo.typeText(username, "SnowWhite");
 		solo.typeText(password, "SevenDwarfs");
 		solo.clickOnButton("Log in using Tequila");
 		
-		//Envoyer la requete avec le mockHttpClient
 		
 		getActivityAndWaitFor(TTChecks.MAIN_ACTIVITY_SHOWN);
 		
