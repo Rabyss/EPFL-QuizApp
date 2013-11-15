d6f258ad68c9072868bf9cebc93cae32ff73d078
Sacha Bron <sacha.bron@copperflake.com>
2013-11-15 15:57:27 +0100

    Adding query() and on(..) to the SearchQuery class.

--- SwEng2013QuizApp/src/epfl/sweng/searchquestions/SearchQuery.java
+++ SwEng2013QuizApp/src/epfl/sweng/searchquestions/SearchQuery.java
@@ -2,14 +2,30 @@ package epfl.sweng.searchquestions;
 
 import static epfl.sweng.util.StringHelper.containsNonWhitespaceCharacters;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import epfl.sweng.quizquestions.QuizQuestion;
+import epfl.sweng.servercomm.RequestContext;
+import epfl.sweng.servercomm.ServerCommunicator;
+
 public class SearchQuery {
     private final String mQuery;
 
     private static final String QUERY_CHAR_CLASS_REGEX = "^[a-zA-Z0-9\\(\\)\\*\\+ ]+$";
     private static final int MAX_QUERY_LENGTH = 500;
+    
+	private static final int SWENG_OK = 200;
 
     public SearchQuery(final String query) throws UnvalidSearchQueryException {
-        
         int errorsCount = auditQueryStr(query);
         if (errorsCount > 0) {
             throw new UnvalidSearchQueryException(errorsCount);
@@ -71,5 +87,49 @@ public class SearchQuery {
                     + mQuery + "'");
         }
     }
-
+    
+    public void query() {
+		ArrayList<BasicNameValuePair> params = new ArrayList<BasicNameValuePair>();
+		params.add(new BasicNameValuePair("query", this.mQuery));
+		
+		try {
+			UrlEncodedFormEntity entity;
+			entity = new UrlEncodedFormEntity(params);
+			RequestContext req = new RequestContext("https://sweng-quiz.appspot.com/search", entity);
+			req.addHeader(entity.getContentType());
+			
+            ServerCommunicator.getInstance().doHttpPost(req, new ServerQueryEvent());
+		} catch (UnsupportedEncodingException e) {
+			this.error("Error: Unsupported Encoding Exception.");
+		}
+	}
+    
+	public void on(ServerQueryEvent event) {
+		int status = event.getStatus();
+		
+		if (status == SWENG_OK) {
+			try {
+				String json = event.getJSONQuestions();
+				JSONObject jsonObj = new JSONObject(json);
+		        
+				JSONArray questionArray = jsonObj.getJSONArray("questions");
+				Set<QuizQuestion> questionSet = new HashSet<QuizQuestion>();
+				
+				for (int i = 0; i<questionArray.length(); i++) {
+					JSONQuestion jsonQuestion = new JSONQuestion(questionArray.getJSONObject(i));
+					questionSet.add(jsonQuestion.getQuizQuestion());
+				}
+				
+		        // this.emit(new XXXEvent(questionSet));
+			} catch (JSONException e) {
+				this.error("Error: malformed JSON (session).");
+			}
+		} else {
+			this.error("Error "+status+" on Tequila Server.");
+		}
+	}
+	
+	public void error(String err) {
+		// TODO implement error
+	}
 }
