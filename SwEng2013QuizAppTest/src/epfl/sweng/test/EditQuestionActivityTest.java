39dd6db1d4f9c07be6c8efec6e5ea51793e30f17
Andy Roulin <andy.roulin@epfl.ch>
2013-11-16 23:46:32 +0100

    Correcting testBadSubmit

--- SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
@@ -151,7 +151,7 @@ public class EditQuestionActivityTest
         assertTrue("Submit button not found.", solo.searchButton(SUBMIT_BUTTON_TEXT));
         solo.clickOnButton(SUBMIT_BUTTON_TEXT);
      
-        getActivityAndWaitFor(TTChecks.OFFLINE_CHECKBOX_ENABLED);
+        getActivityAndWaitFor(TTChecks.NEW_QUESTION_SUBMITTED);
         assertTrue("Error message not found.", solo.searchText(FETCHING_ERROR_MESSAGE));
        
         //subject to uncomment
