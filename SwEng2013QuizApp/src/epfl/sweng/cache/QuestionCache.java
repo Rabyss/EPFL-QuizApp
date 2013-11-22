9c6b0b2bf07a0c863b31cc975cd456a490cf1a77
Andy Roulin <andy.roulin@epfl.ch>
2013-11-22 15:37:48 +0100

    Implements constructor of QuestionCache

--- SwEng2013QuizApp/src/epfl/sweng/cache/QuestionCache.java
+++ SwEng2013QuizApp/src/epfl/sweng/cache/QuestionCache.java
@@ -1,16 +1,40 @@
 package epfl.sweng.cache;
 
+import java.io.File;
 import java.util.Set;
+
+import android.content.Context;
 
 import epfl.sweng.quizquestions.QuizQuestion;
 
-public class QuestionCache {
+public final class QuestionCache {
 	
+	/**
+	 * Singleton instance of QuestionCache
+	 */
 	private static QuestionCache instance = null;
 	
-	public static synchronized QuestionCache getInstance() {
+	/**
+	 * Directory where tag file are stored
+	 */
+	private File tagDir;
+	
+	/**
+	 * Directory where questions are stored
+	 */
+	private File questionDir;
+	
+	private QuestionCache(Context context) {
+		File mainDir = context.getFilesDir();
+		tagDir = new File(mainDir, "tags");
+		tagDir.mkdir(); // return false if already exists
+		questionDir = new File(mainDir, "questions");
+		questionDir.mkdir();
+	}
+
+	public static synchronized QuestionCache getInstance(Context context) {
 		if (instance == null) {
-			instance = new QuestionCache();
+			instance = new QuestionCache(context);
 		}
 		return instance;
 	}
