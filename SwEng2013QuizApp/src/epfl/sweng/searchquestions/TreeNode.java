854b17b279e46b8ec5575bd205e37a4549039322
Philémon Favrod <philemonf@gmail.com>
2013-11-29 01:33:43 +0100

    Delete TreeNode.java

--- SwEng2013QuizApp/src/epfl/sweng/searchquestions/TreeNode.java
+++
@@ -1,9 +0,0 @@
-package epfl.sweng.searchquestions;
-
-import java.util.Set;
-
-public abstract class TreeNode {
-	public abstract Set<Integer> getIDs();
-	public abstract TreeNode getLeftChild();
-	public abstract TreeNode getRightChild();
-}
