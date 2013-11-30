2d3fe83f8aca3bd3db1de5e2a6b5be7fce83aca2
Philémon Favrod <philemonfavrod@MacBook-Pro-de-Philemon-Favrod.local>
2013-11-30 14:30:57 +0100

    sql compiler

--- SwEng2013QuizApp/src/epfl/sweng/searchquestions/parser/SQLQueryCompiler.java
+++ SwEng2013QuizApp/src/epfl/sweng/searchquestions/parser/SQLQueryCompiler.java
@@ -4,29 +4,39 @@ import epfl.sweng.searchquestions.parser.tree.ASTVisitor;
 import epfl.sweng.searchquestions.parser.tree.SingleChildTreeNode;
 import epfl.sweng.searchquestions.parser.tree.TreeAnd;
 import epfl.sweng.searchquestions.parser.tree.TreeLeaf;
+import epfl.sweng.searchquestions.parser.tree.TreeNode;
 import epfl.sweng.searchquestions.parser.tree.TreeOr;
 
 /**
  * Transforms an AST to a SQL request.
  */
 public class SQLQueryCompiler implements ASTVisitor {
+
+    public String toSQL(TreeNode root) {
+        return root.accept(this);
+    }
+
     @Override
     public String visit(TreeLeaf leaf) {
-        return leaf.getTag();
+        return "tag = '"+leaf.getTag()+"'";
     }
 
     @Override
     public String visit(TreeAnd andNode) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return visitTreeNodeWithTwoChild("AND", andNode);
     }
 
     @Override
     public String visit(TreeOr orNode) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return visitTreeNodeWithTwoChild("OR", orNode);
     }
 
     @Override
     public String visit(SingleChildTreeNode scNode) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return scNode.getChild(0).accept(this);
+    }
+
+    private String visitTreeNodeWithTwoChild(String operand, TreeNode node) {
+        return "("+node.getChild(0).accept(this)+" "+operand+" "+node.getChild(1).accept(this)+")";
     }
 }
