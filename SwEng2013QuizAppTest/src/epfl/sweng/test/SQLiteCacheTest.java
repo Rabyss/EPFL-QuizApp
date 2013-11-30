91512f955575d16dc65620e26399fec17e316ed7
Andy Roulin <andy.roulin@epfl.ch>
2013-11-30 15:11:06 +0100

    Tags cannot have _

--- SwEng2013QuizAppTest/src/epfl/sweng/test/SQLiteCacheTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/SQLiteCacheTest.java
@@ -26,21 +26,21 @@ public class SQLiteCacheTest extends AndroidTestCase {
 		String[] answersArray = {"Answer a", "Answer b", "Answer c"};
 		List<String> answers = Arrays.asList(answersArray);
 		Set<String> tags = new HashSet<String>();
-		tags.add("Tag_a"); tags.add("Tag_b");
+		tags.add("Taga"); tags.add("Tagb");
 		qu1 = 
 				new QuizQuestion("Question 1", answers, 1, tags, 1212, "owner");
 		
 		String[] answersArray2 = {"Answer 1", "Answer 2", "Answer 3"};
 		List<String> answers2 = Arrays.asList(answersArray2);
 		Set<String> tags2 = new HashSet<String>();
-		tags2.add("Tag_a"); tags2.add("Tag_c");
+		tags2.add("Taga"); tags2.add("Tagc");
 		qu2 = 
 				new QuizQuestion("Question 2", answers2, 1, tags2, 2424, "otherOwner");
 	}
 	
 	public void testSearchOneTagQuestion() {
 		cache.cacheQuestion(qu1);
-		QueryParserResult res = QueryParser.parse("tag_a");
+		QueryParserResult res = QueryParser.parse("Taga");
 		assertTrue(res.isDone());
 		assertTrue(cache.getQuestionSetByTag(res.getAST()).contains(qu1));
 	}
